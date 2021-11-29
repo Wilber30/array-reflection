3cdbf4c8296818095fd7966bfc185c14f1fd894c
Wilber30 <wilbersears@gmail.com>
2021-11-29 12:05:32 +0000

    - Email validation is working.

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+
+// Email Validation
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const error = document.getElementById("error");
+
+// messages accepts string
+form.addEventListener('submit', (e) => {
+  let messages = []
+
+  // If user has entered email/ else if, then compares value to regex format
+    if (email.value === '' || email.value == null) {
+      messages.push('Please enter an email address')
+    } else if (!isEmail(email.value)) {
+      messages.push('Invalid email address')
+    }
+    // If  message length  >  0, prevent submit & display messages in error div
+  if (messages.length > 0) {
+    e.preventDefault()
+    error.innerText = messages.join(', ')
+  }
+})
+
+// Function tests variable against regex format
+function isEmail(i) {
+  return /^([a-z\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/.test(i);
+}
