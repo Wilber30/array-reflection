3098c56e9022dd985b3399af5724aaa0050fffdd
Wilber30 <wilbersears@gmail.com>
2021-11-29 17:37:00 +0000

    - Added random image program.

--- JS/main.js
+++ JS/main.js
@@ -25,3 +25,22 @@ form.addEventListener('submit', (e) => {
 function isEmail(i) {
   return /^([a-z\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/.test(i);
 }
+
+const requestURL = "https://api.unsplash.com/search/photos?query=random&client_id=y-_yh_bYyoUgOO9Eq6u4Ov4smojskd0HzkgQL1zg2Ys";
+const getImageBtn = document.getElementById("generate_btn");
+const imgToDisplay = document.getElementById("imageToDisplay");
+
+getImageBtn.addEventListener('click', async () => {
+  let randomImage = await getNewImage();
+  imgToDisplay.src = randomImage;
+})
+
+async function getNewImage() {
+    let randomNumber = Math.floor(Math.random() * 10);
+    return fetch(requestURL)
+    .then((response) => response.json())
+    .then((data) => {
+      let allImages = data.results[randomNumber];
+      return.allImages.urls.regular;
+  });
+}
