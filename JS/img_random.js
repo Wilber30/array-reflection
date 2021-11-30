3ed00c6256cff400084d3ef99683e904e946a77c
Wilber30 <wilbersears@gmail.com>
2021-11-30 09:21:27 +0000

    - JS program displays promise object in console.

--- JS/img_random.js
+++ JS/img_random.js
@@ -1,18 +1,11 @@
-const requestURL = "https://api.unsplash.com/search/photos?query=random&client_id=y-_yh_bYyoUgOO9Eq6u4Ov4smojskd0HzkgQL1zg2Ys";
+
+// Random image
+
+const requestURL = `https://api.unsplash.com/photos/random/?client_id=y-_yh_bYyoUgOO9Eq6u4Ov4smojskd0HzkgQL1zg2Ys`;
 const getImageBtn = document.getElementById("generate_btn");
 const imgToDisplay = document.getElementById("imageToDisplay");
 
-getImageBtn.addEventListener('click', async () => {
-  let randomImage = await getNewImage();
-  imgToDisplay.src = randomImage;
+fetch(requestURL)
+.then(function (response) {
+  console.log(response.json());
 })
-
-async function getNewImage() {
-    let randomNumber = Math.floor(Math.random() * 10);
-    return fetch(requestURL)
-    .then((response) => response.json())
-    .then((data) => {
-      let allImages = data.results[randomNumber];
-      return.allImages.urls.regular;
-  });
-}
